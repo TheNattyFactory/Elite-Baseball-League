009ed1ac74c55e57e7072f9fcfea87dd9b9b9ab9
Thomas <thomas.garner1344@yahoo.com>
2026-09-01 12:46:58 -0400

    Update STATIC path to include 'static' directory

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from email.message import EmailMessage
 
 ROOT=os.path.dirname(os.path.abspath(__file__))
 DB=os.environ.get("EBL_DB_PATH", os.path.join(ROOT,"ebl.db"))
-STATIC=ROOT
+STATIC=os.path.join(ROOT,"static")
 SESSIONS={}
 R=random.Random(7500831)
 
