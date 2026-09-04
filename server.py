1a7157eb4f8f5f5755a29e2e52006df6076c572f
Thomas <thomas.garner1344@yahoo.com>
2026-09-04 16:03:37 -0400

    Refactor slot update logic in server.py

--- server.py
+++ server.py
@@ -2388,13 +2388,13 @@ class H(BaseHTTPRequestHandler):
                     (pid,)
                 ).fetchall()
 
-             for slot in slots:
-                c.execute(
+            for slot in slots:
+                    c.execute("""
                         UPDATE roster_slots
                         SET player_id=NULL,occupant_type='OPEN'
                         WHERE franchise_id=? AND slot_no=?
-                        ,(slot["franchise_id"],slot["slot_no"]))
-                        restored_slots+=1
+                    """,(slot["franchise_id"],slot["slot_no"]))
+                    restored_slots+=1
 
                 c.execute("DELETE FROM players WHERE id=?",(pid,))
                 removed_players+=1
