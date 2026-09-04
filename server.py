dc01147e7b923b6fe4736f655dc51d6377b53ed3
Thomas <thomas.garner1344@yahoo.com>
2026-09-04 14:43:53 -0400

    Fix SQL execution syntax for updating roster slots

--- server.py
+++ server.py
@@ -2447,11 +2447,11 @@ class H(BaseHTTPRequestHandler):
                 ).fetchall()
 
              for slot in slots:
-                c.execute("""
+                c.execute(
                         UPDATE roster_slots
                         SET player_id=NULL,occupant_type='OPEN'
                         WHERE franchise_id=? AND slot_no=?
-                        """,(slot["franchise_id"],slot["slot_no"]))
+                        ,(slot["franchise_id"],slot["slot_no"]))
                         restored_slots+=1
 
                 c.execute("DELETE FROM players WHERE id=?",(pid,))
